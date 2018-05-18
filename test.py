580ef3d4c4964911537ea89ae0956050d074e524
spencerdrak <spencerdrak@gmail.com>
2018-05-17 23:15:43 -0400

    removed test file

--- test.py
+++
@@ -1,4 +0,0 @@
-with open("spencerComp.reg", 'rb') as source_file:
-  with open("test.txt", 'w+b') as dest_file:
-    contents = source_file.read()
-    dest_file.write(contents.decode('utf-16').encode('utf-8'))
